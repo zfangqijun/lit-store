3a5063341a22646e4576e6dfd914b50d019cb74a
zfangqijun <zfangqj@gmail.com>
2022-11-02 01:55:22 +0800

    Update lit-store.d.ts

--- lit-store.d.ts
+++ lit-store.d.ts
@@ -1,11 +1,22 @@
 import { EventEmitter } from 'events';
+
 declare class Store<State extends object> extends EventEmitter {
     private state;
     constructor(initialState: State);
     setState(name: keyof State, value: any): void;
     getState(name: keyof State): any;
 }
+
 declare function select(storeStateName: string): (element: Element, name: string) => void;
-declare function connect<State extends object>(store: Store<State>): <T extends new (...a: any[]) => any>(superClass: T) => T;
+
+declare function connect<State extends object>(store: Store<State>): <T extends new (...a: any[]) => any>(superClass: T) => {
+  new (...a: any[]): {
+      [x: string]: any;
+      readonly store: Store<State>;
+      connectedCallback(): void;
+  };
+} & T;
+
 declare function createStore<State extends object>(initialState: State): Store<State>;
+
 export { connect, select, createStore };
